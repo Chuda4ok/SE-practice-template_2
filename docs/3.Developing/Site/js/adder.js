fb3c22c5ecf3f5046ffd40a4849f27b0de272b0d
Vlad Palko <106401702+Stickki@users.noreply.github.com>
2023-03-15 17:54:46 +0100

    Add files via upload

--- docs/3.Developing/Site/js/adder.js
+++ docs/3.Developing/Site/js/adder.js
@@ -1,5 +1,10 @@
+// import items from './db.json' assert {type: 'json'};
+
 function showGroup(group) {
   document.querySelector(".inputBoxList").value = group;
+}
+function showSurname(surname) {
+  document.querySelector(".boxName").value = surname;
 }
 
 let dropdown = document.querySelector(".groupList1");
@@ -25,6 +30,8 @@ settingsIcon.onclick = function () {
 const inputBoxList = document.querySelector(".inputBoxList");
 const dropListElement = document.querySelectorAll(".dropListElement");
 const boxName = document.querySelector(".boxName");
+
+
 inputBoxList.addEventListener("click", () => {
   inputBoxList.classList.toggle("inputClick");
 });
@@ -34,6 +41,73 @@ boxName.addEventListener("click", () => {
 dropListElement.forEach((elem) => {
   elem.addEventListener("click", () => {
     inputBoxList.classList.remove("inputClick");
-    boxName.classList.remove("inputClick");
   });
 });
+
+const withdrawalCard = () => {
+  let printContainet = document.querySelector("#print-container");
+  let date = document.querySelector("#date").value;
+  let grup = document.querySelector("#grup").value;
+  let teach = document.querySelector("#teach").value;
+  let nameString = document.querySelector("#surname").value;
+
+  let divPrint = document.createElement("div");
+  divPrint.classList.add("print-page");
+  // a.style.background = color;
+  divPrint.innerHTML = `<div class="xz">дата проведення:${date}<br>номер нупи №${grup}<br>викладач:${teach}<br>кому:${nameString}</div>`;
+  printContainet.append(divPrint);
+};
+
+document.querySelector("#acceptButton").onclick = withdrawalCard;
+
+const dropBlockList2 = async () => {
+  let response = await fetch('js/surnames.json');
+  let arr = await response.json();
+  let dropBlockList2 = document.querySelector('.dropBlockList2');
+  let listElements = dropBlockList2.querySelectorAll('.dropListElement');
+
+  // Проверяем наличие элементов в списке
+  if (listElements.length === 0) {
+    for (let i = 0; i < arr.length; i++) {
+      let divList = document.createElement('div');
+      divList.classList.add('dropListElement');
+      divList.innerHTML = `${arr[i]}`;
+      divList.addEventListener('click', () => {
+        showSurname(arr[i]);
+      });
+      dropBlockList2.append(divList);
+    }
+  }
+};
+
+if (document.querySelector("#surname")) {
+  document.querySelector("#surname").onclick = dropBlockList2;
+}
+
+
+
+
+//   <div class="dropListElement" onclick="showSurname('Палько')">
+//                   Палько
+//                 </div>
+//                 <div class="dropListElement" onclick="showSurname('Савченко')">
+//                   Савченко
+//                 </div>
+//                 <div class="dropListElement" onclick="showSurname('Цикура')">
+//                   Цикура
+//                 </div>
+//                 <div class="dropListElement" onclick="showSurname('14')">
+//                   Имя родителя
+//                 </div>
+//                 <div class="dropListElement" onclick="showSurname('14')">
+//                   Имя родителя
+//                 </div>
+//                 <div class="dropListElement" onclick="showSurname('14')">
+//                   Имя родителя
+//                 </div>
+//                 <div class="dropListElement" onclick="showSurname('14')">
+//                   Имя родителя
+//                 </div>
+//                 <div class="dropListElement" onclick="showSurname('14')">
+//                   Имя родителя
+//                 </div>
