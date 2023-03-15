a46fb078b93bca52d129c811a0e8aeb0c87e6b28
Vlad Palko <106401702+Stickki@users.noreply.github.com>
2023-03-15 14:44:58 +0100

    Add files via upload

--- docs/3.Developing/Site/js/adder.js
+++ docs/3.Developing/Site/js/adder.js
@@ -1,29 +1,39 @@
-function showGroup(group){
-    document.querySelector('.inputBoxList').value = group;
+function showGroup(group) {
+  document.querySelector(".inputBoxList").value = group;
 }
 
-let dropdown = document.querySelector('.groupList1');
-let dropactive = document.querySelector('.dropBlockList1');
-dropdown.onclick = function(){
-    dropactive.classList.toggle('active');
-}
+let dropdown = document.querySelector(".groupList1");
+let dropactive = document.querySelector(".dropBlockList1");
+dropdown.onclick = function () {
+  dropactive.classList.toggle("active");
+};
 
-let dropdown2 = document.querySelector('.groupList2');
-let dropactive2 = document.querySelector('.dropBlockList2');
-dropdown2.onclick = function(){
-    dropactive2.classList.toggle('active');
-}
+let dropdown2 = document.querySelector(".groupList2");
+let dropactive2 = document.querySelector(".dropBlockList2");
+dropdown2.onclick = function () {
+  dropactive2.classList.toggle("active");
+};
 
-let settingsIcon = document.querySelector('.settingsIcon');
-let settings = document.querySelector('.settings');
-let containerSettings = document.querySelector('.containerSetting');
-settingsIcon.onclick = function(){
-    settings.classList.toggle('activeMenu');
-    containerSettings.classList.toggle('active');
-}
+let settingsIcon = document.querySelector(".settingsIcon");
+let settings = document.querySelector(".settings");
+let containerSettings = document.querySelector(".containerSetting");
+settingsIcon.onclick = function () {
+  settings.classList.toggle("activeMenu");
+  containerSettings.classList.toggle("active");
+};
 
 const inputBoxList = document.querySelector(".inputBoxList");
-inputBoxList.addEventListener('click', () => {
-    inputBoxList.classList.toggle("inputClick")
-})
-
+const dropListElement = document.querySelectorAll(".dropListElement");
+const boxName = document.querySelector(".boxName");
+inputBoxList.addEventListener("click", () => {
+  inputBoxList.classList.toggle("inputClick");
+});
+boxName.addEventListener("click", () => {
+  boxName.classList.toggle("inputClick");
+});
+dropListElement.forEach((elem) => {
+  elem.addEventListener("click", () => {
+    inputBoxList.classList.remove("inputClick");
+    boxName.classList.remove("inputClick");
+  });
+});
